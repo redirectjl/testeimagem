c06ed1076090e1aab0f2a0eaeecd01fe6a142300
Gustavo Luigi <=>
2023-12-12 07:36:28 -0300

    wait for the image to load before scrolling the page

---
+++ js/scroll-to-button.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.style.scrollBehavior = "smooth";
+  document.body.style.scrollSnapType = "mandatory";
+  document.body.style.scrollSnapPointsY = "repeat(100%)";
+
+  setTimeout(() => {
+    const targetSection = document.getElementById("geradorbotao");
+
+    const offset = 11;
+    const targetPosition = window.scrollY + targetSection.getBoundingClientRect().top - offset;
+
+    window.scrollTo({ top: targetPosition, behavior: 'smooth' });
+  }, 500);
+
+  let mainImg = document.querySelector("#main-img");
+
+  if (mainImg.complete) {
+    mainImg.classList.add("show");
+
+    const list = document.querySelector(".list");
+    const footer = document.querySelector(".footer");
+
+    list.classList.remove("v-none");
+    footer.classList.remove("v-none");
+  }
+});
